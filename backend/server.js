555d3a1397a5789c278154be05e6a3fef844e5a7
Ryan Hui <ryanhui30@gmail.com>
2025-05-13 23:21:06 -0700

    Add neon database setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,46 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
+
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from the backend");
-})
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.use("/api/products", productRoutes);
+
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
 });
-
