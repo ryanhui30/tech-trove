7a6b33732dc225b564184ed8c35b6ff717a58d49
Ryan Hui <ryanhui30@gmail.com>
2025-05-14 20:11:42 -0700

    Add delete product functionality

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -31,4 +31,19 @@ export const useProductStore = create((set, get) => ({
       set({ loading: false });
     }
   },
+
+  deleteProduct: async (id) => {
+    console.log("deleteProduct function called", id);
+    set({ loading: true });
+    try {
+      await axios.delete(`${BASE_URL}/api/products/${id}`);
+      set((prev) => ({ products: prev.products.filter((product) => product.id !== id) }));
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      console.log("Error in deleteProduct function", error);
+      toast.error("Something went wrong");
+    } finally {
+      set({ loading: false });
+    }
+  },
 }));
