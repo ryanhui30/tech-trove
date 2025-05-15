0b477bf4ed0a0c43d910349d5d37f9aa3aee02f0
Ryan Hui <ryanhui30@gmail.com>
2025-05-14 19:41:20 -0700

    Add navbar and theme selector components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
+import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
+
+function App() {
+  const {theme} = useThemeStore()
   return (
-    <div>
-      <h1 className="text-red-500">hey</h1>
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
     </div>
   );
 }
